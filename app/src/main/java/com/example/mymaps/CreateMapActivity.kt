353b6b3316bfe1a274aebed8962ea54b5adc59a5
Alexandre Quiblier <quiblier.a@gmail.com>
2021-01-15 18:54:08 +0100

    adding marker on map +sanity checking + alert

--- app/src/main/java/com/example/mymaps/CreateMapActivity.kt
+++ app/src/main/java/com/example/mymaps/CreateMapActivity.kt
@@ -1,8 +1,14 @@
 package com.example.mymaps
 
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -11,6 +17,7 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.material.snackbar.Snackbar
 
 private const val TAG = "CreateMapActivity"
 
@@ -28,17 +35,15 @@ class CreateMapActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        mapFragment.view?.let {
+            Snackbar.make(it, "Long Press to add a Marker", Snackbar.LENGTH_INDEFINITE)
+                .setAction("OK", {})
+                .setActionTextColor(ContextCompat.getColor(this, android.R.color.white))
+                .show()
+        }
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
@@ -50,12 +55,35 @@ class CreateMapActivity : AppCompatActivity(), OnMapReadyCallback {
 
         mMap.setOnMapLongClickListener { latLng ->
             Log.i(TAG, "onMapLongClickListener")
-            val newMarker = mMap.addMarker(MarkerOptions().position(latLng).title("my new Marker").snippet("here is the Snippet"))
-            markers.add(newMarker)
+            showAlertDialog(latLng)
         }
         // Add a marker in Sydney and move the camera
         val sydney = LatLng(-34.0, 151.0)
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+    private fun showAlertDialog(latLng: LatLng) {
+        val placeForView = LayoutInflater.from(this).inflate(R.layout.dialog_create_place, null)
+        val dialog =
+            AlertDialog
+            .Builder(this)
+            .setTitle("Create a Marker")
+            .setView(placeForView)
+            .setNegativeButton("Cancel", null)
+            .setPositiveButton("OK", null)
+                .show()
+
+        dialog.getButton(DialogInterface.BUTTON_POSITIVE).setOnClickListener {
+            val title = placeForView.findViewById<EditText>(R.id.etTitle).text.toString()
+            val description = placeForView.findViewById<EditText>(R.id.etDescription).text.toString()
+            if (title.trim().isEmpty() || description.trim().isEmpty()) {
+                Toast.makeText(this, "Place must have non-empty title and description", Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+            val newMarker = mMap.addMarker(MarkerOptions().position(latLng).title(title).snippet(description))
+            markers.add(newMarker)
+            dialog.dismiss()
+        }
     }
 }
